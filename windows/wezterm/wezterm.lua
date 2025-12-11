ff4f6613d139af0f4e72ac11f27642c1c0bde16d
m-kuwata <kuwatamasaya@gmail.com>
2025-12-11 23:11:57 +0900

    見た目調整

--- windows/wezterm/wezterm.lua
+++ windows/wezterm/wezterm.lua
@@ -10,14 +10,48 @@ if wezterm.config_builder then
     config = wezterm.config_builder()
 end
 
+----------------------------------------------------
+-- general
+----------------------------------------------------
+-- コンフィグ自動反映（Ctrl+Shift+R）
 config.automatically_reload_config = true
+-- デフォルトシェル
+config.default_domain = 'WSL:Ubuntu-24.04'
 
-config.font = wezterm.font("HackGen35 Console NF")
-config.font_size = 14.0
+----------------------------------------------------
+-- Tab
+----------------------------------------------------
+-- タイトルバーを非表示
+config.window_decorations = "RESIZE"
+
+-- タブバーの表示
+config.show_tabs_in_tab_bar = true
+
+-- タブが一つの時は非表示
+-- config.hide_tab_bar_if_only_one_tab = true
+
+-- タブの追加ボタンを非表示
+-- config.show_new_tab_button_in_tab_bar = false.
+
+-- nightlyのみ使用可能
+-- タブの閉じるボタンを非表示
+-- config.show_close_tab_button_in_tabs = false
 
 -- タブを下に表示（デフォルトでは上にある）
 config.tab_bar_at_bottom = true
 
-config.default_domain = 'WSL:Ubuntu-24.04'
+----------------------------------------------------
+-- font
+----------------------------------------------------
+-- フォントファミリー
+config.font = wezterm.font("HackGen35 Console NF")
+
+-- フォントサイズ
+config.font_size = 14.0
+
+----------------------------------------------------
+-- keybinds
+----------------------------------------------------
+
 
 return config
