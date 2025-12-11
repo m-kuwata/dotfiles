b9730249ddb2bfcde2cc5b0b494355b47ee29ea5
m-kuwata <kuwatamasaya@gmail.com>
2025-12-11 22:59:50 +0900

    alias 追加

--- wsl/zsh/alias.zsh
+++ wsl/zsh/alias.zsh
@@ -2,4 +2,6 @@ alias ls="eza --icons -la --group-directories-first"
 alias cat="bat"
 alias ..="cd .."
 alias g="git"
+alias lg="lazygit"
+alias ld="lazydocker"
 alias tree='eza --icons -al -T -L 2'
