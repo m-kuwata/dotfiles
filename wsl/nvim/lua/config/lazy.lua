dfaaa7a80e4576245157ab008ace6c6acd572c5e
m-kuwata <kuwatamasaya@gmail.com>
2026-01-19 22:47:45 +0900

    go設定追加

--- wsl/nvim/lua/config/lazy.lua
+++ wsl/nvim/lua/config/lazy.lua
@@ -1,22 +1,21 @@
 -- lazy.nvim bootstrap
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- LazyVim
 require("lazy").setup({
-  {
-    "LazyVim/LazyVim",
-    import = "lazyvim.plugins",
-  },
+	{
+		"LazyVim/LazyVim",
+		import = "lazyvim.plugins",
+	},
 })
-
